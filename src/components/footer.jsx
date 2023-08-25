7fe6ec6b8a33b2beb9c3aaff2f1682faa9e57304
Wilson Huang <wilsonhuang11012000@gmail.com>
2023-08-25 19:30:18 -0400

    made mobile compatible

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -2,7 +2,7 @@
 const Footer = () =>{
     return(
 <footer>
-    <div className="credits">Created By @whuang1101</div>
+    <div className="credits">Created By @<a href="https://github.com/whuang1101/blog_client" style={{color:"white", textDecoration:"none"}}>whuang1101</a></div>
 </footer>)
 }
 export default Footer
